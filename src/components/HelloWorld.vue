e225a1bc2dd6405e6c705c214a9448388c21cd3c
paritosh baghel <paritoshmmmec@gmail.com>
2020-12-07 21:15:23 -0500

    fix

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,74 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div class="p-grid">
+      <div class="p-col-2"></div>
+      <div class="p-col-8">
+        <div>
+          <h3>{{ message }}</h3>
+        </div>
+      </div>
+      <div class="p-col-2"></div>
+    </div>
+    <div class="p-grid">
+      <div class="p-col-2"></div>
+      <div class="p-col-8">
+        <Card>
+          <template #content>
+            <DataTable :value="cars">
+              <Column field="vin" header="Endpoint Name"></Column>
+              <Column field="year" header="Year"></Column>
+              <Column field="brand" header="Brand"></Column>
+              <Column field="color" header="Color"></Column>
+            </DataTable>
+          </template>
+        </Card>
+      </div>
+      <div class="p-col-2"></div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+  data() {
+    return {
+      message: "WayPi - API Catalog",
+      endpoints: [
+        {
+          id: 1,
+          name: "Country Names",
+          ownedBy: "Cost Team",
+          url: "https://api.openaq.org/v1/countries",
+          description: "See history of data update attempts by the platform.",
+        },
+        {
+          id: 2,
+          name: "API Name 1",
+          ownedBy: "Cost Team",
+          url: "https://api.openaq.org/v1/fetches",
+          description: "See history of data update attempts by the platform.",
+        },
+      ],
+      cars: [
+        { brand: "Volkswagen", year: 2012, color: "Orange", vin: "dsad231ff" },
+        { brand: "Audi", year: 2011, color: "Black", vin: "gwregre345" },
+        { brand: "Renault", year: 2005, color: "Gray", vin: "h354htr" },
+        { brand: "BMW", year: 2003, color: "Blue", vin: "j6w54qgh" },
+        { brand: "Mercedes", year: 1995, color: "Orange", vin: "hrtwy34" },
+        { brand: "Volvo", year: 2005, color: "Black", vin: "jejtyj" },
+        { brand: "Honda", year: 2012, color: "Yellow", vin: "g43gr" },
+        { brand: "Jaguar", year: 2013, color: "Orange", vin: "greg34" },
+        { brand: "Ford", year: 2000, color: "Black", vin: "h54hw5" },
+        { brand: "Fiat", year: 2013, color: "Red", vin: "245t2s" },
+      ],
+    };
+  },
+  methods: {
+    reverseMessage() {
+      this.message = this.message.split("").reverse().join("");
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
